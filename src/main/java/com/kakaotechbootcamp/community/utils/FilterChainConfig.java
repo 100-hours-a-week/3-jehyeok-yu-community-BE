626cea082f09d71e8e1da03c85ae58ac85be06c4
유제혁 <wpgur07@gmail.com>
2025-11-02 23:15:13 +0900

    feat: filter의 jwt 에러핸들링을 위한 filter 작성
    - jwtFilter Error를 FilterAuthException으로 정의 후 json으로 변환하여 반환하도록 구현
    - filterChain에 추가하는 구문 작성

--- src/main/java/com/kakaotechbootcamp/community/utils/FilterChainConfig.java
+++ src/main/java/com/kakaotechbootcamp/community/utils/FilterChainConfig.java
@@ -1,6 +1,7 @@
 package com.kakaotechbootcamp.community.utils;
 
 import com.kakaotechbootcamp.community.utils.security.filter.CorsFilter;
+import com.kakaotechbootcamp.community.utils.security.filter.ErrorHandlingFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,16 @@ public class FilterChainConfig {
         filterRegistrationBean.setFilter(corsFilter);
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ErrorHandlingFilter> registrationExceptionHandlingFilter(
+        ErrorHandlingFilter errorHandlingFilter) {
+        FilterRegistrationBean<ErrorHandlingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(errorHandlingFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(2);
         return filterRegistrationBean;
     }
 }
