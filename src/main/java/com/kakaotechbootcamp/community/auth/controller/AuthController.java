12a3ee9a0bfbc15df7a7a8a3ba06b75946758d6c
유제혁 <wpgur07@gmail.com>
2025-11-03 00:06:13 +0900

    feat: jwt에 맞춰 AuthService, Controller, 예외 구현
    - jwt에 맞게 Controller 작성
    - Login성공시 바디에 accessToken을 반환하도록 수정
    - refresh Api에서 쿠키에서 refreshToken을 빼오도록 파라미터 작성
    - 성공시 accessToken반환하도록 작성
    - requestContext를 사용해 userId를 빼오도록 구현
    
    - jwt에 맞게 Service 작성
    - accessToken은 바디 refreshToken은 setCookie헤더에 담아 반환하도록 구현
    - httpOnly, path는 auth로만 가도록 구성, maxAge는 별도 설정
    - token필드는 null은 로그아웃, 만료는 시간체크
    - 예외는 accessToken/refreshToken 만료시 다른 에러를 반환
    - refreshToken 만료시에만 로그아웃을 유발하도록 고정된 예외 반환

--- src/main/java/com/kakaotechbootcamp/community/auth/controller/AuthController.java
+++ src/main/java/com/kakaotechbootcamp/community/auth/controller/AuthController.java
@@ -1,17 +1,17 @@
 package com.kakaotechbootcamp.community.auth.controller;
 
 import com.kakaotechbootcamp.community.auth.dto.request.LoginRequestDto;
+import com.kakaotechbootcamp.community.auth.dto.response.LoginResponseDto;
 import com.kakaotechbootcamp.community.auth.service.AuthService;
-import com.kakaotechbootcamp.community.utils.exception.customexception.NotImplementException;
 import com.kakaotechbootcamp.community.utils.response.ApiResponse;
-import com.kakaotechbootcamp.community.utils.security.AuthPrincipal;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,21 +24,24 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<Void>> login(HttpServletResponse httpResponse,
+    public ResponseEntity<ApiResponse<LoginResponseDto>> login(HttpServletResponse response,
         @RequestBody LoginRequestDto requestDto) {
-        authService.login(httpResponse, requestDto);
-        return ResponseEntity.ok(ApiResponse.ok(null));
+        LoginResponseDto responseDto = authService.login(response, requestDto);
+        return ResponseEntity.ok(ApiResponse.ok(responseDto));
     }
 
     @PutMapping
-    public ResponseEntity<ApiResponse<Void>> refresh() {
-        throw new NotImplementException();
+    public ResponseEntity<ApiResponse<LoginResponseDto>> refresh(
+        @CookieValue("refreshToken") String refreshToken
+        , HttpServletResponse response) {
+        LoginResponseDto responseDto = authService.refresh(refreshToken, response);
+        return ResponseEntity.ok(ApiResponse.ok(responseDto));
     }
 
     @DeleteMapping
     public ResponseEntity<ApiResponse<Void>> logout(
-        @AuthenticationPrincipal AuthPrincipal authPrincipal) {
-        authService.logout(authPrincipal.getUserId());
+        @RequestAttribute("userId") Long userId, HttpServletResponse response) {
+        authService.logout(userId, response);
         return ResponseEntity.ok(ApiResponse.ok(null));
     }
 }
--- src/main/java/com/kakaotechbootcamp/community/auth/service/AuthService.java
+++ src/main/java/com/kakaotechbootcamp/community/auth/service/AuthService.java
@@ -1,19 +1,23 @@
 package com.kakaotechbootcamp.community.auth.service;
 
 import com.kakaotechbootcamp.community.auth.dto.request.LoginRequestDto;
+import com.kakaotechbootcamp.community.auth.dto.response.LoginResponseDto;
 import com.kakaotechbootcamp.community.auth.entity.Refreshtoken;
+import com.kakaotechbootcamp.community.auth.exception.LoginRequiredException;
 import com.kakaotechbootcamp.community.auth.exception.UserNotFoundException;
 import com.kakaotechbootcamp.community.auth.repository.RefreshtokenRepository;
 import com.kakaotechbootcamp.community.user.entity.User;
 import com.kakaotechbootcamp.community.user.repository.UserRepository;
 import com.kakaotechbootcamp.community.utils.security.JwtTokenProvider;
+import com.kakaotechbootcamp.community.utils.security.dto.AccessTokenPayload;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletResponse;
-import java.security.SecureRandom;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanInitializationException;
@@ -32,10 +36,9 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
-    private final PasswordEncoder encoder;
     private final RefreshtokenRepository refreshtokenRepository;
+    private final PasswordEncoder encoder;
     private final Clock clock;
-    private final SecureRandom secureRandom = new SecureRandom();
     @Value("#{environment['security.jwt.refresh-ttl-millis']}")
     private long refreshTtlMillis;
 
@@ -46,53 +49,79 @@ public class AuthService {
         }
     }
 
-    public void login(HttpServletResponse httpResponse, LoginRequestDto requestDto) {
+    public LoginResponseDto login(HttpServletResponse httpResponse, LoginRequestDto requestDto) {
         User user = userRepository.findByEmail(requestDto.getEmail())
             .orElseThrow(UserNotFoundException::new);
         if (!encoder.matches(requestDto.getRawPassword(), user.getPassword())) {
             throw new UserNotFoundException();
         }
 
-        String refreshToken = Base64.getUrlEncoder().withoutPadding()
-            .encodeToString(generateRefreshtoken());
-        String accessToken = jwtTokenProvider.createAccessToken(user.getUserId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(user.getUserId());
 
-        // localhost에서 사용하기 위한 주석처리.
+        // 이미지 구현 시 image 경로 넣는 것 필요함.
+        String accessToken = jwtTokenProvider.createAccessToken(user.getUserId(),
+            new AccessTokenPayload(
+                user.getNickname(), "default"));
+
         ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", refreshToken)
-            .path("/")
             .httpOnly(true)
-//            .secure(true)
             .sameSite("Lax")
+            .path("/auth")
             .maxAge(Duration.ofMillis(refreshTtlMillis))
             .build();
 
-        httpResponse.addHeader("Authorization", "Bearer " + accessToken);
         httpResponse.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
-        String newRefreshHash = encoder.encode(refreshToken);
         Instant newExpires = clock.instant().plusMillis(refreshTtlMillis);
 
         Refreshtoken token = refreshtokenRepository.findByUser(user)
             .orElse(null);
 
         if (token == null) {
-            token = Refreshtoken.create(user, newRefreshHash, newExpires);
+            token = Refreshtoken.create(user, refreshToken, newExpires);
         } else {
-            token.replaceHash(newRefreshHash, newExpires);
+            token.replaceHash(refreshToken, newExpires);
         }
 
         refreshtokenRepository.save(token);
+        return new LoginResponseDto(accessToken);
     }
 
-    private byte[] generateRefreshtoken() {
-        byte[] bytes = new byte[32];
-        secureRandom.nextBytes(bytes);
-        return bytes;
+    public LoginResponseDto refresh(String refreshToken, HttpServletResponse response) {
+        final Jws<Claims> parsedRefreshToken;
+        try {
+            parsedRefreshToken = jwtTokenProvider.parse(refreshToken);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new LoginRequiredException();
+        }
+        long userId = Long.parseLong(parsedRefreshToken.getBody().getSubject());
+
+        Refreshtoken entity = refreshtokenRepository.findByUser_UserId(userId)
+            .orElseThrow(LoginRequiredException::new);
+
+        if (entity.getRefreshtoken() == null
+            || !entity.getRefreshtoken().equals(refreshToken)
+            || entity.isExpired(clock.instant())) {
+            throw new LoginRequiredException();
+        }
+
+        User user = entity.getUser();
+        String newAccessToken = jwtTokenProvider.createAccessToken(user.getUserId(),
+            new AccessTokenPayload(user.getNickname()));
+
+        return new LoginResponseDto(newAccessToken);
     }
 
     @Transactional
-    public void logout(long userId) {
+    public void logout(long userId, HttpServletResponse httpResponse) {
         refreshtokenRepository.findByUser_UserId(userId)
             .ifPresent(Refreshtoken::revokeNow);
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", "")
+            .path("/auth")
+            .maxAge(0)
+            .build();
+
+        // 리스폰스 명시적 제거
+        httpResponse.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
     }
 }
