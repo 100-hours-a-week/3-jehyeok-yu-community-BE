0920626ed456071751f61c37dfdc2b6ab58210e0
유제혁 <wpgur07@gmail.com>
2025-11-06 17:34:02 +0900

    chore: cors 필터에서 awsPublicDomain 주입 받도록 설정

--- src/main/java/com/kakaotechbootcamp/community/utils/security/filter/CorsFilter.java
+++ src/main/java/com/kakaotechbootcamp/community/utils/security/filter/CorsFilter.java
@@ -1,11 +1,13 @@
 package com.kakaotechbootcamp.community.utils.security.filter;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +15,13 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class CorsFilter extends OncePerRequestFilter {
 
     private final List<String> allowedOrigin = List.of("http://localhost:3000");
+    @Value("${aws.public.domain}")
+    private String awsPublicDomain;
+
+    @PostConstruct
+    void init() {
+        allowedOrigin.add(awsPublicDomain + ":3000");
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
